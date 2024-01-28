3ce207751ab5c2a0d9dcb909fe79b6ad665dae17
RuxandraRambet <ruxandraramb@gmail.com>
2024-01-28 20:12:54 +0100

    feat: laravel template

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+// inclusione css 
+import '~resources/scss/app.scss';
+// process img
+import.meta.glob(['../img/**']);
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
